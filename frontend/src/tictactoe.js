c5c76a31005478a834788a69cef3a68fc07cefb6
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-09-22 19:30:19 +0530

    added event listener for cells

--- frontend/src/tictactoe.js
+++ frontend/src/tictactoe.js
@@ -3,5 +3,14 @@ import { Board } from "./board";
 export class TicTacToe extends Board {
     constructor(el) {
         super(el);
+        this.bindEvents();
+    }
+
+    bindEvents() {
+        this.el.addEventListener("click", (e) => {
+            const cell = e.target.dataset["cell"];
+
+            console.log(cell);
+        });
     }
 }
