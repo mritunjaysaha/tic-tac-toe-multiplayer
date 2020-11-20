8d9f66af9b86f4eba8dea4f96c66fbca667285a0
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-11-20 18:05:14 +0530

    updated performance

--- frontend/build-utils/webpack.production.js
+++ frontend/build-utils/webpack.production.js
@@ -1,5 +1,4 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = () => ({
     output: {
@@ -12,10 +11,6 @@ module.exports = () => ({
                 use: [MiniCssExtractPlugin.loader, "css-loader"],
             },
         ],
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [new CssMinimizerPlugin()],
     },
 
     plugins: [new MiniCssExtractPlugin()],
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpackMerge = require("webpack-merge");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 const modeConfig = (env) => require(`./build-utils/webpack.${env}`)(env);
 const presetConfig = require("./build-utils/loadPresets");
@@ -37,6 +38,10 @@ module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
                         use: ["style-loader", "css-loader", "sass-loader"],
                     },
                 ],
+            },
+            optimization: {
+                minimize: true,
+                minimizer: [new CssMinimizerPlugin()],
             },
             output: {
                 filename: "bundle.js",
