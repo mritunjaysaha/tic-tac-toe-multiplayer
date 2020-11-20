0633badbb296894a6567c1ada3ee2d8644c85f7b
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-11-20 15:02:50 +0530

    minified css

--- frontend/build-utils/webpack.production.js
+++ frontend/build-utils/webpack.production.js
@@ -12,5 +12,10 @@ module.exports = () => ({
             },
         ],
     },
+    optimization: {
+        minimize: true,
+        minimizer: [new CssMinimizerPlugin()],
+    },
+
     plugins: [new MiniCssExtractPlugin()],
 });
