0e7ba8d6e94f83ad9d8d0dbda2f8f2648eb5f534
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-12-06 23:58:51 +0530

    removed redundant test

--- server/test/test1.test.js
+++ server/test/test1.test.js
@@ -20,14 +20,6 @@ test("check winner should return 2", () => {
     expect(checkWinner(arr, player1Moves, player2Moves)).toEqual(2);
 });
 
-test("check winner should return 2", () => {
-    const player1Moves = 2;
-    const player2Moves = 3;
-    const arr = [2, 2, 2, 1, 1, null, null, null, null];
-
-    expect(checkWinner(arr, player1Moves, player2Moves)).toEqual(2);
-});
-
 test("check winner should return draw", () => {
     const player1Moves = 4;
     const player2Moves = 5;
